cdbaed9c24fbdd946bec809259ed8583b495577a
Literal Genie <frothsavd@gmail.com>
2022-06-19 17:31:15 -0400

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { config } from 'src/config/config';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    fetch() {}
+    constructor(private http: HttpClient) {}
+
+    test = this.http.get(config.serverUrl + 'series/ids');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        MatCardModule,
+        BrowserAnimationsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
